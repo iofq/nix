94ebce046be91502f96255de065bdb3719187ab4
iofq <cjriddz@protonmail.com>
2024-01-27 14:56:29 -0600

    microvm testing

--- nixos/htz/vms.nix
+++ nixos/htz/vms.nix
@@ -1,6 +1,8 @@
-{ ... }: {
+{ self, ... }: {
   microvm.vms = {
     vm-test = {
+      flake = self;
+      updateFlake = "github:iofq/nix";
       config = {
         microvm.shares = [{
           source = "/nix/store";
@@ -8,6 +10,13 @@
           tag = "ro-store";
           proto = "virtiofs";
         }];
+        microvm.interfaces = [
+        {
+          type = "tap";
+          id = "vm-test";
+          mac = "02:00:00:00:00:01";
+        }
+        ];
         system.stateVersion = "23.11"; 
       };
     };
